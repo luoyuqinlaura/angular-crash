6e811c2be99c4e06b08f888538ae03c0c079489f
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-03-15 10:50:46 -0400

    angular condition

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -10,9 +10,9 @@ export class SalesPersonListComponent implements OnInit {
   // create an array of person objects
   salesPersonList: SalesPerson[] = [
     new SalesPerson('Laura', 'Luo', 'kdsfhglkd@gamil.com', 235435),
-    new SalesPerson('Laura', 'Luo', 'kdsfhglkd@gamil.com', 235435),
-    new SalesPerson('Laura', 'Luo', 'kdsfhglkd@gamil.com', 235435),
-    new SalesPerson('Laura', 'Luo', 'kdsfhglkd@gamil.com', 235435),
+    new SalesPerson('Yuqin', 'Luo', 'gfjfghd@gamil.com', 335),
+    new SalesPerson('Annie', 'Chen', 'dsfhglkd@gamil.com', 567),
+    new SalesPerson('Lola', 'Xu', 'xud@gamil.com', 8435),
   ];
 
   constructor() {}
